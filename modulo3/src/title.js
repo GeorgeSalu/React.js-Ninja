46798d3a2e71a4541376a60d1dac1d52ffcd1b4a
George Salu <george.salu10@gmail.com>
2019-09-25 22:54:58 -0300

    getDefaultProps

--- modulo3/src/title.js
+++ modulo3/src/title.js
@@ -3,6 +3,11 @@
 import React from 'react'
 
 const Title = React.createClass({
+    getDefaultProps: function() {
+        return {
+            name: 'Desconhecido'
+        }
+    },
     render: function() {
         return (
             <h1>Ola {this.props.name}</h1>
