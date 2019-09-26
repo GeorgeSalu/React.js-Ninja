0d4df990667bc58589b44d8a640edcbe14a61e5c
George Salu <george.salu10@gmail.com>
2019-09-25 23:12:13 -0300

    passando outros tipos de dados via props

--- modulo3/src/title.js
+++ modulo3/src/title.js
@@ -5,12 +5,13 @@ import React from 'react'
 const Title = React.createClass({
     getDefaultProps: function() {
         return {
-            name: 'Desconhecido'
+            name: 'Desconhecido',
+            lastName: 'Desconhecido'
         }
     },
     render: function() {
         return (
-            <h1>Ola {this.props.name}</h1>
+            <h1>Ola {this.props.name + ' - ' + this.props.lastName}</h1>
         )
     }
 })
