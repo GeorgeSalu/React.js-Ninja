36fc2a9aec761973226de1846000943d7313b4f1
George Salu <george.salu10@gmail.com>
2019-09-26 22:23:18 -0300

    atributos html

--- modulo3/src/app.js
+++ modulo3/src/app.js
@@ -6,8 +6,10 @@ import Title from './title'
 var App = React.createClass({
   render: function () {
     return (
-        <div>
+        <div className="1">
             <Title name='Fernando Daciuk'/>
+            <label htmlFor='input'>Input</label>
+            <input type='text' id="input" />
         </div>
     )
   }
