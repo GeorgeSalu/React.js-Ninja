994ae316eca2b78b49afff1933237dfd55982614
George Salu <george.salu10@gmail.com>
2019-10-01 10:12:29 -0300

    state

--- modulo3/src/app.js
+++ modulo3/src/app.js
@@ -1,15 +1,20 @@
 'use strict'
 
 import React, { Component } from 'react'
-import LikeButton from './like-button'
-import SearchButton from './search-button'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      text: 'Fernando'
+    }
+  }
   render() {
     return (
-      <div className='container' >
-          <LikeButton />
-          <SearchButton />
+      <div className='container' onClick={() => this.setState({
+        text: 'outro texto'
+      })} >
+        {this.state.text}
       </div>
     )
   }
