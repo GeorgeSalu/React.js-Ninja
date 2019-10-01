8d3e01b50a9b0e155023ce16ddc229bfef3dda35
George Salu <george.salu10@gmail.com>
2019-10-01 11:34:49 -0300

    lifecycle: fluxo de montagem/desmontagem

--- modulo3/src/app.js
+++ modulo3/src/app.js
@@ -3,26 +3,35 @@
 import React, { Component } from 'react'
 import Button from './button'
 import Square from './square'
+import Timer from './timer'
 
 class App extends Component {
   constructor() {
+    console.log('constructor')
     super()
     this.state = {
-      color: 'green'
+      showTimer: true
     }
   }
+
+  componentWillMount() {
+    console.log('componentWillMount')
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+  }
+
   render() {
     return (
       <div >
-        <Square color={this.state.color}/>
-        
-        {['red','green','blue'].map((color) => (
-          <Button
-            key={color}
-            handleClick={() => this.setState({ color })}>
-              {color}
-          </Button>
-        ))}
+        {this.state.showTimer && <Timer />}
+
+        <button onClick={() => {
+          this.setState({ showTimer: !this.state.showTimer })
+        }}>
+          Show / Hide timer
+        </button>
       </div>
     )
   }
