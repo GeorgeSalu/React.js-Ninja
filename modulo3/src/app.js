24b9341ade537b1afe84981fdb3072d545600588
George Salu <george.salu10@gmail.com>
2019-10-01 16:34:16 -0300

    introducao a formularios com react

--- modulo3/src/app.js
+++ modulo3/src/app.js
@@ -1,16 +1,15 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Button from './button'
 
 class App extends Component {
 
   render() {
     return (
       <div >
-        <Button handleClick={() => console.log('clicou')}>
-          Clique aqui 
-        </Button>
+        <form>
+          <input type='text' defaultValue='valor inicial'/>
+        </form>
       </div>
     )
   }
