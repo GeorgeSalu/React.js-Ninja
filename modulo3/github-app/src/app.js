df679209e47d11dee17ec05a48fb16d7720108b7
George Salu <george.salu10@gmail.com>
2019-10-02 11:20:19 -0300

    criando a marcação da aplicacao

--- modulo3/github-app/src/app.js
+++ modulo3/github-app/src/app.js
@@ -2,10 +2,51 @@
 
 import React from 'react';
 
-const App = React.createClass({
-    render: function() {
-        return <h1>Titulo react</h1>
-    }
-})
+const App = () => (
+  <div className='app'>
+
+    <div className='search'>
+      <input
+        type='search'
+        placeholder='Digite o nome do usuario do github'/>
+    </div>
+
+    <div className='user-info'>
+      <img src='https://avatars.githubusercontent.com/u/48766?v=3' />
+      <h2>
+        <a href='https://github.com/fdaciuk'>Fernando Daciuk</a>
+      </h2>
+
+      <ul className='repos-info'>
+        <li>- Repositorios: 122</li>
+        <li>- Seguidores: 10</li>
+        <li>- Seguindo: 10</li>
+      </ul>
+
+      <div className='actions'>
+        <button>Ver Repositorios</button>
+        <button>Ver Favoritos</button>
+      </div>
+
+      <div className='repos'>
+        <h2>Repositorios</h2>
+        <ul>
+          <li>
+            <a href='#'>Nome do repositorio</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className='starred'>
+        <h2>Favoritos</h2>
+        <ul>
+          <li>
+            <a href='#'>Nome do repositorio</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+)
 
 export default App
