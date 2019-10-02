c7922d09b5effd7a88f749ac8d1bc1236cef90f2
George Salu <george.salu10@gmail.com>
2019-10-02 12:13:02 -0300

    separando em componentes

--- modulo3/github-app/src/app.js
+++ modulo3/github-app/src/app.js
@@ -1,41 +1,21 @@
 'use strict'
 
 import React from 'react';
+import Search from './components/search'
+import UserInfo from './components/user-info'
+import Actions from './components/actions'
+import Repos from './components/repos'
 
 const App = () => (
   <div className='app'>
 
-    <div className='search'>
-      <input
-        type='search'
-        placeholder='Digite o nome do usuario do github'/>
-    </div>
+    <Search />
 
-    <div className='user-info'>
-      <img src='https://avatars.githubusercontent.com/u/48766?v=3' />
-      <h1 className='username'>
-        <a href='https://github.com/fdaciuk'>Fernando Daciuk</a>
-      </h1>
+    <UserInfo />
 
-      <ul className='repos-info'>
-        <li>- Repositorios: 122</li>
-        <li>- Seguidores: 10</li>
-        <li>- Seguindo: 10</li>
-      </ul>
+    <Actions />
 
-      <div className='actions'>
-        <button>Ver Repositorios</button>
-        <button>Ver Favoritos</button>
-      </div>
 
-      <div className='repos'>
-        <h2>Repositorios</h2>
-        <ul>
-          <li>
-            <a href='#'>Nome do repositorio</a>
-          </li>
-        </ul>
-      </div>
 
       <div className='starred'>
         <h2>Favoritos</h2>
@@ -46,7 +26,6 @@ const App = () => (
         </ul>
       </div>
     </div>
-  </div>
 )
 
 export default App
---
+++ modulo3/github-app/src/components/repos.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import React from 'react'
+
+const Repos = () => (
+  <div className='repos'>
+    <h2>Repositorios</h2>
+    <ul>
+      <li>
+        <a href='#'>Nome do repositorio</a>
+      </li>
+    </ul>
+  </div>
+)
+
+export default Repos
