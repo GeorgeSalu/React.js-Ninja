4ff3719ebb774401f56a45ec8a1ea613ec4c230b
George Salu <george.salu10@gmail.com>
2019-10-02 15:12:03 -0300

    importando lib ajax

--- modulo3/github-app/src/components/search.js
+++ modulo3/github-app/src/components/search.js
@@ -6,7 +6,11 @@ const Search = () => (
   <div className='search'>
     <input
       type='search'
-      placeholder='Digite o nome do usuario do github'/>
+      placeholder='Digite o nome do usuario do github'
+      onChange={(e) => {
+        console.log('change', e.target.value)
+      }}
+      />
   </div>
 )
 
