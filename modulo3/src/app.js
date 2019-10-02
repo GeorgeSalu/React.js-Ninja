79f67daa1a0c82bf5543688c503402899100bb64
George Salu <george.salu10@gmail.com>
2019-10-02 09:46:33 -0300

    introducao a formularios (checkbox)

--- modulo3/src/app.js
+++ modulo3/src/app.js
@@ -3,12 +3,31 @@
 import React, { Component } from 'react'
 
 class App extends Component {
-
+  constructor() {
+    super()
+    this.state = {
+      value: 'valor inicial',
+      checked: false
+    }
+  }
   render() {
     return (
       <div >
         <form>
-          <input type='text' defaultValue='valor inicial'/>
+          <input type='text' value={this.state.value} onChange={(e) => {
+            this.setState({ value: e.target.value })
+          }}/>
+
+          <label>
+            <input
+              type='checkbox'
+              value='my-checkbox'
+              checked={this.state.checked}
+              onChange={(e) => {
+                this.setState({ checked: !this.state.checked })
+              }} />
+              checkbox
+          </label>
         </form>
       </div>
     )
