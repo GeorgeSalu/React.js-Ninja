1cdfca570246a5e32b7bb267a0469dda1c3d8167
George Salu <george.salu10@gmail.com>
2019-10-02 09:56:36 -0300

    introducao a formularios (select e option)

--- modulo3/src/app.js
+++ modulo3/src/app.js
@@ -6,31 +6,22 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      value: 'valor inicial',
-      checked: false
+      value: '2'
     }
   }
   render() {
     return (
       <div >
         <form>
-          <input type='text' value={this.state.value} onChange={(e) => {
-            this.setState({ value: e.target.value })
-          }}/>
-
-          <label>
-            <input
-              type='checkbox'
-              value='my-checkbox'
-              checked={this.state.checked}
-              onChange={(e) => {
-                this.setState({ checked: !this.state.checked })
-              }} />
-              checkbox
-          </label>
-
-          <input type='radio' name='rd' value='1' defaultChecked />Radio 1
-          <input type='radio' name='rd' value='2' />Radio 2
+          <select value={this.state.value} onChange={(e) => {
+            this.setState({
+              value: e.target.value
+            })
+          }}>
+            <option value='1'>Opcao 1</option>
+            <option value='2'>Opcao 2</option>
+            <option value='3'>Opcao 3</option>
+          </select>
         </form>
       </div>
     )
