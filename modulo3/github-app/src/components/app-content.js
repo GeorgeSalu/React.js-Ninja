499634120981f0594194d999b1e5be0795e660fd
George Salu <george.salu10@gmail.com>
2019-10-05 16:40:37 -0300

    completando os propTypes em app-content

--- modulo3/github-app/src/components/app-content.js
+++ modulo3/github-app/src/components/app-content.js
@@ -38,7 +38,11 @@ const AppContent = ({ userinfo, repos, starred, handleSearch, getRepos, getStarr
 AppContent.propTypes = {
   userinfo: PropTypes.object,
   repos: PropTypes.array.isRequired,
-  starred: PropTypes.array.isRequired
+  starred: PropTypes.array.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  handleSearch: PropTypes.func.isRequired,
+  getRepos: PropTypes.func.isRequired,
+  getStarred: PropTypes.func.isRequired
 }
 
 export default AppContent
