d29a0693b797b8e25b6fd2c7d7647a806ad72fae
George Salu <george.salu10@gmail.com>
2019-10-07 15:51:53 -0300

    jest na pratica - conhecendo as funcoes para teste e assercao

--- secao4 - modulo 2/jest-in-practice/sum.test.js
+++ secao4 - modulo 2/jest-in-practice/sum.test.js
@@ -1,5 +1,19 @@
 'use strict'
 
-it('Testando jest', () => {
-    
+describe('# escopo', () => {
+  it('Um é igual a 1', () => {
+    expect(1).toBe(1)
+  })
+  it('Testando jest 2', () => {
+
+  })
+})
+
+describe('# escopo 2', () => {
+  it('Testando jest', () => {
+
+  })
+  it('Testando jest 2', () => {
+
+  })
 })
