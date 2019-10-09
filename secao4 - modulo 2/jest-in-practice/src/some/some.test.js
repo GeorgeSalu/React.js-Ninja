bc880d429eee9ffe8a5193cbafbdfb2a61dbf1df
George Salu <george.salu10@gmail.com>
2019-10-09 17:11:41 -0300

    TDD - some - parte 2

--- secao4 - modulo 2/jest-in-practice/src/some/some.test.js
+++ secao4 - modulo 2/jest-in-practice/src/some/some.test.js
@@ -18,3 +18,11 @@ it('some([1,2], (item) => item) should return true', () => {
 it('some([1,2,3], (item, index) => index % 2 === 0) should return true', () => {
   expect(some([1,2,3], (item, index) => index % 2 === 0)).to.be.ok
 })
+
+it('some([1,3,5], (item) => item % 2 === 0) should return false', () => {
+  expect(some([1,3,5], (item) => item % 2 === 0)).to.not.be.ok
+})
+
+it('some([1,2,3], (item, index, array) => array.indexOf(2) === 1) should return true', () => {
+  expect(some([1,2,3], (item, index, array) => array.indexOf(2) === 1)).to.be.ok
+})
