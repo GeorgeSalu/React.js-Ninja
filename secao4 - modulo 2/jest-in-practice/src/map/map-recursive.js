53a531185cc13aa139b302f471d266fecf8450fb
George Salu <george.salu10@gmail.com>
2019-10-08 22:21:28 -0300

    map recursivo

---
+++ secao4 - modulo 2/jest-in-practice/src/map/map-recursive.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const map = (arr, func) => {
+
+  if(typeof func !== 'function')
+    throw new TypeError('func is not a function')
+
+  if(!Array.isArray(arr))
+    throw new TypeError('arr is not an array')
+
+  let newArr = []
+  for(let i = 0;i < arr.length; i++) {
+    newArr.push(func(arr[i], i, arr))
+  }
+  return newArr
+}
+
+export default map
