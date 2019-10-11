a79a773e15b9070fa57907ecededc41657e7e650
George Salu <george.salu10@gmail.com>
2019-10-11 16:28:03 -0300

    start projeto github-app

---
+++ secao4 - modulo 2/github-app/src/app.js
@@ -0,0 +1,89 @@
+'use strict'
+
+import React, { Component } from 'react';
+import AppContent from './components/app-content'
+
+import ajax from '@fdaciuk/ajax'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      userinfo: null,
+      repos: [],
+      starred: [],
+      isFetching: false
+    }
+
+    this.handleSearch = this.handleSearch.bind(this)
+  }
+
+  getGitHubApiUrl(username, type) {
+    const internalUser = username ? `/${username}` : ''
+    const internalType = type ? `/${type}` : ''
+    return `https://api.github.com/users${internalUser}${internalType}`
+  }
+
+  handleSearch(e) {
+    const value = e.target.value
+    const keyCode = e.which || e.keyCode
+    const ENTER = 13
+
+    e.persist()
+    if(keyCode === ENTER) {
+      this.setState({
+        isFetching: true
+      })
+
+      ajax().get(this.getGitHubApiUrl(value))
+        .then((result) => {
+          this.setState({
+            userinfo: {
+              username: result.name,
+              photo: result.avatar_url,
+              login: result.login,
+              repos: result.public_repos,
+              followers: result.followers,
+              following: result.following
+            },
+            repos:[],
+            starred: []
+          })
+
+        })
+        .always(() => this.setState({ isFetching: false }))
+    }
+
+  }
+
+  getRepos(type) {
+    return (e) => {
+      const username = this.state.userinfo.login
+      ajax().get(this.getGitHubApiUrl(username, type))
+        .then((result) => {
+          this.setState({
+            [type]: result.map((repo) => {
+              return {
+                name: repo.name,
+                link: repo.html_url
+              }
+            })
+          })
+        })
+    }
+  }
+
+  render() {
+    return <AppContent
+        userinfo={this.state.userinfo}
+        repos={this.state.repos}
+        starred={this.state.starred}
+        handleSearch={this.handleSearch}
+        getRepos={this.getRepos('repos')}
+        getStarred={this.getRepos('starred')}
+        isFetching={this.state.isFetching}
+     />
+  }
+}
+
+export default App
---
+++ secao4 - modulo 2/github-app/src/components/search.js
@@ -0,0 +1,21 @@
+'use strict'
+
+import React from 'react'
+
+const Search = ({ isDisabled, handleSearch }) => (
+  <div className='search'>
+    <input
+      type='search'
+      placeholder='Digite o nome do usuario do github'
+      disabled={isDisabled}
+      onKeyUp={handleSearch}
+      />
+  </div>
+)
+
+Search.propTypes = {
+  handleSearch: React.PropTypes.func.isRequired,
+  isDisabled: React.PropTypes.bool.isRequired
+}
+
+export default Search
