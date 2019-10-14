f2c0c170068e53f565a1824f5dc7964fc2651b51
George Salu <george.salu10@gmail.com>
2019-10-14 12:50:11 -0300

    modularizando todos os componetes

--- secao4 - modulo 2/github-app/src/components/app-content.js
+++ secao4 - modulo 2/github-app/src/components/app-content.js
@@ -1,10 +1,10 @@
 'use strict'
 
 import React, {PropTypes} from 'react'
-import Search from './search'
-import UserInfo from './user-info'
-import Actions from './actions'
-import Repos from './repos'
+import Search from './search/search'
+import UserInfo from './user-info/user-info'
+import Actions from './actions/actions'
+import Repos from './repos/repos'
 
 const AppContent = ({ userinfo, repos, starred, handleSearch, getRepos, getStarred, isFetching }) => (
     <div className='app'>
--- secao4 - modulo 2/github-app/webpack.prod.config.js
+++ secao4 - modulo 2/github-app/webpack.prod.config.js
@@ -2,59 +2,76 @@
 
 const path = require('path')
 const webpack = require('webpack')
+const validate = require('webpack-validator')
 
 const HtmlPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-module.exports = {
-    entry: [
-        path.join(__dirname, 'src', 'index')
-    ],
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: '[name]-[hash].js',
-    },
+const crp = new ExtractTextPlugin('crp.css')
+const styles = new ExtractTextPlugin('[name]-[hash].css')
 
-    plugins: [
-        new ExtractTextPlugin('style.css'),
+module.exports = validate({
+  entry: path.join(__dirname, 'src', 'index'),
 
-        new webpack.DefinePlugin({
-          'process.env': {
-            'NODE_ENV': '"production"'
-          }
-        }),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name]-[hash].js'
+  },
 
-        new webpack.optimize.UglifyJsPlugin({
-          compress: {warnings: false}
-        }),
+  plugins: [
+    crp,
+    styles,
 
-        new webpack.optimize.DedupePlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': '"production"'
+      }
+    }),
 
-        new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    }),
 
-        new HtmlPlugin({
-          title: 'GitHub app',
-          template: path.join(__dirname, 'src','html','template.html')
-        })
-    ],
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
 
-    module: {
-        preLoaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            include: /src/,
-            loader: 'standard'
-        }],
-        loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            include: /src/,
-            loader: 'babel'
-        },{
-          test: /\.css$/,
-          exclude: /node_modules/,
-          include: /src/,
-          loader: ExtractTextPlugin.extract( 'style','css')
-        }]
+    new HtmlPlugin({
+      title: 'GitHub app',
+      inject: false,
+      template: path.join(__dirname, 'src', 'html', 'template.html')
+    })
+  ],
+
+  module: {
+    preLoaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      include: /src/,
+      loader: 'standard'
+    }],
+
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      include: /src/,
+      loader: 'babel'
+    }, {
+      test: /\.css$/,
+      exclude: /node_modules|(search|style)\.css/,
+      include: /src/,
+      loader: styles.extract('style', 'css')
+    }, {
+      test: /(search|style)\.css$/,
+      exclude: /node_modules/,
+      include: /src/,
+      loader: crp.extract('style', 'css')
+    }]
+  },
+
+  resolve: {
+    alias: {
+      src: path.join(__dirname, 'src'),
+      components: path.join(__dirname, 'src', 'components')
     }
-}
+  }
+})
