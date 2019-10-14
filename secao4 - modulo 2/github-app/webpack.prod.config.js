2aa8df319b508b974aff8d39c394de004a67054c
George Salu <george.salu10@gmail.com>
2019-10-14 11:52:13 -0300

    webpack: gerando bundle de producao

--- secao4 - modulo 2/github-app/webpack.prod.config.js
+++ secao4 - modulo 2/github-app/webpack.prod.config.js
@@ -7,22 +7,31 @@ const HtmlPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-    devtool: 'source-map',
     entry: [
-        'react-hot-loader/patch',
-        'webpack-dev-server/client?http://localhost:3000',
-        'webpack/hot/only-dev-server',
         path.join(__dirname, 'src', 'index')
     ],
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: ''
+        filename: '[name]-[hash].js',
     },
 
     plugins: [
-        new webpack.HotModuleReplacementPlugin(),
         new ExtractTextPlugin('style.css'),
+
+        new webpack.DefinePlugin({
+          'process.env': {
+            'NODE_ENV': '"production"'
+          }
+        }),
+
+        new webpack.optimize.UglifyJsPlugin({
+          compress: {warnings: false}
+        }),
+
+        new webpack.optimize.DedupePlugin(),
+
+        new webpack.optimize.OccurrenceOrderPlugin(),
+
         new HtmlPlugin({
           title: 'GitHub app',
           template: path.join(__dirname, 'src','html','template.html')
@@ -45,7 +54,7 @@ module.exports = {
           test: /\.css$/,
           exclude: /node_modules/,
           include: /src/,
-          loaders: [ 'style','css']
+          loader: ExtractTextPlugin.extract( 'style','css')
         }]
     }
 }
