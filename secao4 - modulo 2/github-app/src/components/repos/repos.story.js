bb25c91cc28fbe4d436243b906a9808ce84f11df
George Salu <george.salu10@gmail.com>
2019-10-15 15:34:15 -0300

    configurando o storybook

---
+++ secao4 - modulo 2/github-app/src/components/repos/repos.story.js
@@ -0,0 +1,9 @@
+'use strict'
+
+import { storiesOf, module } from '@kadira/storybook'
+import React from 'react'
+
+storiesOf('respos',module)
+  .add('first repos story', () => (
+    <div>Repos</div>
+  ))
