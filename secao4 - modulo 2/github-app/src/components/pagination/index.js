e727e7aceef23dcc47cc79af95dc1ca4292e0d13
George Salu <george.salu10@gmail.com>
2019-10-19 12:31:10 -0300

    Paginacao - storybook - parte 3

--- secao4 - modulo 2/github-app/src/components/pagination/index.js
+++ secao4 - modulo 2/github-app/src/components/pagination/index.js
@@ -8,10 +8,21 @@ const Pagination = ({ total, activePage, pageLink }) => (
   <ul>
     {pagination({ total, activePage }).map((page, index) => (
       <li key={index}>
-        <Page page={page} pageLink={pageLink.replace('%page%', page)}/>
+        <Page page={page} pageLink={pageLink.replace('%page%', page)} onClick={onClick}/>
       </li>
     ))}
   </ul>
 )
+
+Pagination.defaultProps = {
+  pageLink: ''
+}
+
+Pagination.propTypes = {
+  total: PropTypes.number,
+  activePage: PropTypes.number,
+  pageLink: PropTypes.string,
+  onClick: PropTypes.func
+}
 
 export default Pagination
--- secao4 - modulo 2/github-app/src/components/pagination/page.js
+++ secao4 - modulo 2/github-app/src/components/pagination/page.js
@@ -5,8 +5,14 @@ import { React } from "react";
 const Page = ({ page, pageLink }) => {
   const Component = page === '...' ? 'span' : 'a'
   const href = page === '...' ? null : pageLink
+
+  const handleClick = !onClick ? null : (e) => {
+    e.preventDefault()
+    onClick(page)
+  }
+
   return (
-    <Component href={href}>
+    <Component href={href} onClick={handleClick}>
       {page}
     </Component>
   )
