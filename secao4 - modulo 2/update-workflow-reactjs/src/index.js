31cd582c6b70da3e10b4fc76d1b81f79e7c2d46c
George Salu <george.salu10@gmail.com>
2019-10-23 11:34:44 -0300

    promise

--- secao4 - modulo 2/update-workflow-reactjs/src/index.js
+++ secao4 - modulo 2/update-workflow-reactjs/src/index.js
@@ -1,24 +1,9 @@
 'use strict'
 
-import React from 'react'
-import { render } from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
-import App from './app'
+const promise = new Promise((resolve, reject) => {
+  resolve('resolvida')
+})
 
-const renderApp = (NextApp) => {
-  render(
-    <AppContainer>
-      <NextApp />
-    </AppContainer>,
-    document.querySelector('[data-js="app"]')
-  )
-}
-
-renderApp(App)
-
-if (module.hot) {
-  module.hot.accept('./app', () => {
-    const NextApp = require('./app').default
-    renderApp(NextApp)
-  })
-}
+promise
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error))
