f81c549ddc89f2721ff5adf82fce9f398d4a5887
George Salu <george.salu10@gmail.com>
2019-10-30 15:04:34 -0300

    incluindo comentario

--- secao4 - modulo 2/markdown-editor/webpack/prod.config.js
+++ secao4 - modulo 2/markdown-editor/webpack/prod.config.js
@@ -6,6 +6,7 @@ const common = require('./common')
 const HtmlPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const CleanPlugin = require('clean-webpack-plugin')
+// plugin do webpack para analisar bundle de produção
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = {
