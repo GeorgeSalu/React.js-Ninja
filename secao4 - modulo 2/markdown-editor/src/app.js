0d4c30474eb96ae536f8e4e012a4ff1068e5c281
George Salu <george.salu10@gmail.com>
2019-10-30 10:57:45 -0300

    Aplicação: editor de Markdow

--- secao4 - modulo 2/markdown-editor/src/app.js
+++ secao4 - modulo 2/markdown-editor/src/app.js
@@ -5,12 +5,26 @@ import React,{ Component } from 'react'
 import './css/style.css'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = { value: '' }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    this.setState({
+      value: e.target.textarea.value
+    })
+  }
+
   render() {
     return (
       <div className='editor'>
-        <form>
-          <textarea />
-          <button >Renderixar markup</button>
+        <form onSubmit={this.handleSubmit}>
+          <textarea name='textarea'/>
+          <button type='submit'>Renderixar markup</button>
         </form>
 
         <div className='view' />
