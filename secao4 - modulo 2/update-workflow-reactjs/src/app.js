8ece08f7b6fa25be302a25785de782b5c20b030c
George Salu <george.salu10@gmail.com>
2019-10-30 09:51:30 -0300

    this.setState() - usando funcoes fazer atualizacoes em lote

--- secao4 - modulo 2/update-workflow-reactjs/src/app.js
+++ secao4 - modulo 2/update-workflow-reactjs/src/app.js
@@ -5,30 +5,17 @@ import React, { Component } from 'react'
 class App extends Component {
   constructor () {
     super()
-    this.state = {
-      title: '...',
-      Component: 'div'
-    }
+    this.state = { counter: 0 }
   }
 
-  getTitle () {
-    return new Promise((resolve, reject) => {
-      resolve('My app with Promisse')
-    })
-  }
-
-  async componentDidMount () {
-    //import dinamico
-    //const title = await import('components/title')
-
-    this.setState({ title: await this.getTitle() })
+  componentDidMount () {
+    this.setState({ counter: this.state.counter + 1 })
   }
 
   render () {
+    const { counter } = this.state
     return (
-      <div>
-        <this.state.Component>{this.state.title}</this.state.Component>
-      </div>
+      <div>counter: {counter}</div>
     )
   }
 }
