68d74a85b3a586f70c90511fb5e56ae66c6de161
George Salu <george.salu10@gmail.com>
2019-10-31 12:02:06 -0300

    removendo espacos

--- secao4 - modulo 2/markdown-editor/src/components/mrkdown-editor.js
+++ secao4 - modulo 2/markdown-editor/src/components/mrkdown-editor.js
@@ -5,11 +5,8 @@ import Header from './markdown-editor-header'
 
 const MarkdownEditor = ({ value, handleChange, getMarkup, isSaving }) => (
   <section className='editor'>
-
     <Header isSaving={isSaving} />
-
     <textarea name='textarea' value={value} onChange={handleChange} autoFocus />
-
     <article className='view' dangerouslySetInnerHTML={getMarkup()} />
   </section>
 )
