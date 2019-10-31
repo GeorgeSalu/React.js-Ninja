f4270cd270663d06a4911a7f361bb07a45c25213
George Salu <george.salu10@gmail.com>
2019-10-31 10:37:06 -0300

    inicio - persistencia de dados

--- secao4 - modulo 2/markdown-editor/src/mrkdown-editor.js
+++ secao4 - modulo 2/markdown-editor/src/mrkdown-editor.js
@@ -4,6 +4,8 @@ import React, { PropTypes } from 'react'
 
 const MarkdownEditor = ({ value, handleChange, getMarkup }) => (
   <div className='editor'>
+    <button onClick={() => localStorage.setItem('md', value)}>Salvar</button>
+
     <textarea name='textarea' value={value} onChange={handleChange} autoFocus />
     <button type='submit'>Renderixar markup</button>
 
