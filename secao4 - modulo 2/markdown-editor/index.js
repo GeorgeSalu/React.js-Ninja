2c34bef644e429709b8e6e84c841222500df480e
George Salu <george.salu10@gmail.com>
2019-10-31 09:54:01 -0300

    Aplicação: editor de Markdown - servindo arquivos comprimidos com gzip

--- secao4 - modulo 2/markdown-editor/index.js
+++ secao4 - modulo 2/markdown-editor/index.js
@@ -1,8 +1,12 @@
 'use strict'
 
-const { join }
+const { join } = require('path')
 const express = require('express')
+const compression = require('compression')
 const app = express()
+
+app.use(compression())
+app.use(express.static(join(__dirname, 'dist')))
 
 app.get('*', (req, res) => {
   res.sendFile(join(__dirname, 'dist', 'index.html'))
