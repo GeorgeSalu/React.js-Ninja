d216d2c27dbf98099ac9559e3a8ff63fb91e8df1
George Salu <george.salu10@gmail.com>
2019-11-03 10:47:47 -0300

    Aplicação: editor markdown - mas oportunidade de refactory

---
+++ secao4 - modulo 2/markdown-editor/src/views/index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+import React, { PropTypes } from 'react'
+import Header from './header'
+
+const MarkdownEditor = ({ value, handleChange, getMarkup, textareaRef, ...props }) => (
+  <section className='editor'>
+    <Header {...props} />
+    <textarea name='textarea' value={value} onChange={handleChange} autoFocus ref={textareaRef} />
+    <article className='view' dangerouslySetInnerHTML={getMarkup()} />
+  </section>
+)
+
+MarkdownEditor.propTypes = {
+  value: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  isSaving: PropTypes.bool.isRequired,
+  getMarkup: PropTypes.func.isRequired,
+  textareaRef: PropTypes.func.isRequired
+}
+
+export default MarkdownEditor
