dda42120747b0c6a6a95416f92d4cdc37dc1ecf1
George Salu <george.salu10@gmail.com>
2019-11-05 11:50:21 -0300

    estado inicial do projeto

--- secao4 - modulo 2/pure-component/src/app.js
+++ secao4 - modulo 2/pure-component/src/app.js
@@ -1,30 +1,13 @@
 'use strict'
 
-import React, { PureComponent } from 'react'
+import React,{ Component } from 'react'
 
-import './css/style.css'
-
-class App extends PureComponent {
-  constructor () {
-    super()
-    this.state = {
-      color: 'purple'
-    }
-
-    this.handleClick = () => {
-      this.setState({ color: 'black' })
-    }
-  }
+class App extends Component {
 
   render () {
-    console.log('render')
     return (
       <div>
-        <span>Color name: {this.state.color}</span>
-        <br />
-        <button onClick={this.handleClick}>
-          Change color to black
-        </button>
+        <h1>React</h1>
       </div>
     )
   }
