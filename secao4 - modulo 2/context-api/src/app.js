24f6f029742056e6ff24159b7e29281f17ae37a2
George Salu <george.salu10@gmail.com>
2019-11-05 14:35:53 -0300

    estrutura inicial projeto - context api

--- secao4 - modulo 2/context-api/src/app.js
+++ secao4 - modulo 2/context-api/src/app.js
@@ -5,26 +5,10 @@ import React, { PureComponent } from 'react'
 import './css/style.css'
 
 class App extends PureComponent {
-  constructor () {
-    super()
-    this.state = {
-      color: 'purple'
-    }
-
-    this.handleClick = () => {
-      this.setState({ color: 'black' })
-    }
-  }
-
   render () {
-    console.log('render')
     return (
       <div>
-        <span>Color name: {this.state.color}</span>
-        <br />
-        <button onClick={this.handleClick}>
-          Change color to black
-        </button>
+        <span>React</span>
       </div>
     )
   }
