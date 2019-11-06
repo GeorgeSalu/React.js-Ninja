14f9c869e3e0517216f4007d23ecaf967f6224a2
George Salu <george.salu10@gmail.com>
2019-11-06 09:17:23 -0300

    introdução a context api

--- secao4 - modulo 2/context-api/src/app.js
+++ secao4 - modulo 2/context-api/src/app.js
@@ -1,17 +1,33 @@
 'use strict'
 
 import React, { PureComponent } from 'react'
+import MessageList from 'components/message-list'
+import PropTypes from 'prop-types'
 
 import './css/style.css'
 
 class App extends PureComponent {
+  getChildContext() {
+    return {
+      color: 'purple'
+    }
+  }
+
   render () {
     return (
-      <div>
-        <span>React</span>
-      </div>
+      <MessageList
+        messages={[
+          { text: 'hey' },
+          { text: 'ho' },
+          { text: 'lets go' }
+        ]}
+      />
     )
   }
+}
+
+App.childContextTypes = {
+  color: PropTypes.string
 }
 
 export default App
