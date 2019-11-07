8200e6cdce9c90c7308fd8485173374146c40b09
George Salu <george.salu10@gmail.com>
2019-11-07 14:07:15 -0300

    Context API - isolando o context HOC

---
+++ secao4 - modulo 2/context-api/src/components/with-context.js
@@ -0,0 +1,17 @@
+'use strict'
+
+import React, { PureComponent } from 'react'
+
+const withContext = (Wrapper) => {
+  class WithContext extends PureComponent {
+    render() {
+      return (
+        <Wrapper />
+      )
+    }
+  }
+
+  return WithContext
+}
+
+export default withContext
