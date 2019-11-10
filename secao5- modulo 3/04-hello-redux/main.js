748f6d7249d14d5baa02f3f1e686ba88bbf01370
George Salu <george.salu10@gmail.com>
2019-11-10 17:05:01 -0300

    como funciona o createStore()

---
+++ secao5- modulo 3/04-hello-redux/main.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT': return state + 1
+    case 'DECREMENT': return state - 1
+  }
+
+  return state
+}
+
+const createStore = () => {
+  let state
+  let subscriptions = []
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    subscriptions.forEach((f) => f())
+  }
+
+  const subscribe = (func) => {
+    subscriptions.push(func)
+    return () => {
+      subscriptions = subscriptions.filter((f) => f !== func)
+    }
+  }
+
+  dispatch({})
+
+  return {
+    getState: () => null,
+    dispatch,
+    subscribe
+  }
+}
+const store = createStore(counter)
+
+const $counter = document.querySelector('[data-js="counter"]')
+const $decrement = document.querySelector('[data-js="decrement"]')
+const $increment = document.querySelector('[data-js="increment"]')
+
+$decrement.addEventListener('click', decrement, false)
+$increment.addEventListener('click', increment, false)
+
+function decrement () {
+  store.dispatch({ type: 'DECREMENT' })
+  $decrement.removeEventListener('click', decrement)
+}
+
+function increment () {
+  store.dispatch({ type: 'INCREMENT' })
+}
+
+function render() {
+  $counter.textContent = store.getState()
+}
+
+store.subscribe(render)
+render()
