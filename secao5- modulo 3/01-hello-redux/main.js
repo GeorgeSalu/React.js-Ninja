dff55b92e298d0bee2a71431a92359b29aec1392
George Salu <george.salu10@gmail.com>
2019-11-10 14:14:44 -0300

    criando nosso primeiro reducer com testes

--- secao5- modulo 3/01-hello-redux/main.js
+++ secao5- modulo 3/01-hello-redux/main.js
@@ -1,5 +1,38 @@
 'use strict'
 
+const counter = (state, action) => {
+  if(typeof state === 'undefined') {
+    return 0
+  }
+  if(action.type === 'INCREMENT') {
+    return state + 1
+  }
+  if(action.type === 'DECREMENT') {
+    return state - 1
+  }
+  return state
+}
+
 console.assert(
-  1 == 2
+  counter(0, { type: 'INCREMENT' }) === 1
+)
+
+console.assert(
+  counter(1, { type: 'INCREMENT' }) === 2
+)
+
+console.assert(
+  counter(2, { type: 'DECREMENT' }) === 1
+)
+
+console.assert(
+  counter(5, { type: 'DECREMENT' }) === 4
+)
+
+console.assert(
+  counter(3, { type: 'SOME' }) === 3
+)
+
+console.assert(
+  counter(undefined, {}) === 0
 )
