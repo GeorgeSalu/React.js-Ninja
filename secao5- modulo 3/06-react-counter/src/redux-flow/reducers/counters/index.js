aa7cfda74c8d6046b874e9c0db29365d911bef41
George Salu <george.salu10@gmail.com>
2019-11-12 14:27:16 -0300

    adicionando mas contadores - teste - parte 2

--- secao5- modulo 3/06-react-counter/src/redux-flow/reducers/counters/index.js
+++ secao5- modulo 3/06-react-counter/src/redux-flow/reducers/counters/index.js
@@ -4,7 +4,10 @@ export default (state, action) => {
   switch(action.type) {
     case 'ADD_COUNTER':
       return state.concat(0)
+
     case 'REMOVE_COUNTER':
-      return [0,2]
+      return state.filter((_, index) => {
+        index !== action.index
+      })
   }
 }
