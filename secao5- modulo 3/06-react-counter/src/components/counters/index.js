e66818dacee3a1fdb5a46c7c75ece646936bb78e
George Salu <george.salu10@gmail.com>
2019-11-14 14:47:02 -0300

    Adicionando mas contadores - componetes

--- secao5- modulo 3/06-react-counter/src/components/counters/index.js
+++ secao5- modulo 3/06-react-counter/src/components/counters/index.js
@@ -3,31 +3,32 @@
 import React from 'react'
 import {Counter} from 'components/counter'
 import { connect } from 'react-redux'
+import { ADD_COUNTER } from 'reducers/counters'
 
-const Counters = ({ counter, increment, decrement }) => (
-  <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-    {[0, 0, 0].map((item, index) => (
-      <Counter
-        key={index}
-        counter={counter}
-        increment={increment}
-        decrement={decrement}
-      />
-    ))}
+const Counters = ({ counters, addCounter }) => (
+  <div>
+    <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+      {counters.map((counter, index) => (
+        <Counter
+          key={index}
+          counter={counter}
+        />
+      ))}
+    </div>
+
+    <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: 10 }}>
+      <button onClick={addCounter}>Adicionar contador</button>
+    </div>
   </div>
 )
 
-const mapStateToProps = (state) => {
-  return {
-    counter: state
-  }
-}
+const mapStateToProps = (state) => ({
+  counters: state
+})
 
 const mapDispatchToProps = (dispatch) => ({
-
-  increment: () => dispatch({ type: 'INCREMENT' }),
-  decrement: () => dispatch({ type: 'DECREMENT' })
-
+  addCounter: () => dispatch({ type: ADD_COUNTER })
 })
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counters)
