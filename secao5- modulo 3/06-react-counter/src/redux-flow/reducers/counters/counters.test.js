d8d9e8000c639446922f58614c2b177c0c8e43af
George Salu <george.salu10@gmail.com>
2019-11-14 13:59:02 -0300

    refatoração

--- secao5- modulo 3/06-react-counter/src/redux-flow/reducers/counters/counters.test.js
+++ secao5- modulo 3/06-react-counter/src/redux-flow/reducers/counters/counters.test.js
@@ -3,6 +3,12 @@
 import counters from './index'
 import { expect } from 'chai'
 import deepFreeze from 'deep-freeze'
+import {
+  ADD_COUNTER,
+  REMOVE_COUNTER,
+  INCREMENT,
+  DECREMENT
+} from './index'
 
 it('counters should be a function', () => {
   expect(counters).to.be.a('function')
@@ -10,35 +16,35 @@ it('counters should be a function', () => {
 
 it('should add a counter', () => {
   const before = deepFreeze([])
-  const action = deepFreeze({ type: 'ADD_COUNTER' })
+  const action = deepFreeze({ type: ADD_COUNTER })
   const after = [0]
   expect(counters(before, action)).to.be.deep.equal(after)
 })
 
 it('Should add a new counter again', () => {
   const before = deepFreeze([0, 1])
-  const action = deepFreeze({ type: 'ADD_COUNTER' })
+  const action = deepFreeze({ type: ADD_COUNTER })
   const after = [0, 1, 0]
   expect(counters(before, action)).to.be.deep.equal(after)
 })
 
 it('should remove a counter', () => {
   const before = deepFreeze([0, 1, 2])
-  const action = deepFreeze({ type: 'REMOVE_COUNTER', index: 1 })
+  const action = deepFreeze({ type: REMOVE_COUNTER, index: 1 })
   const after = [0, 2]
   expect(counters(before, action)).to.be.deep.equal(after)
 })
 
 it('should remove a counter again', () => {
   const before = deepFreeze([1, 1])
-  const action = deepFreeze({ type: 'REMOVE_COUNTER', index: 0 })
+  const action = deepFreeze({ type: REMOVE_COUNTER, index: 0 })
   const after = [1]
   expect(counters(before, action)).to.be.deep.equal(after)
 })
 
 it('should increment a counter', () => {
   const before = deepFreeze([0, 0])
-  const action = deepFreeze({ type: 'INCREMENT', index: 0 })
+  const action = deepFreeze({ type: INCREMENT, index: 0 })
   const after = [1, 0]
   expect(counters(before, action)).to.be.deep.equal(after)
 })
@@ -59,7 +65,7 @@ it('should decrement a counter', () => {
 
 it('should decrement an other counter', () => {
   const before = deepFreeze([0, 2, 0])
-  const action = deepFreeze({ type: 'DECREMENT', index: 1 })
+  const action = deepFreeze({ type: DECREMENT, index: 1 })
   const after = [0, 1, 0]
   expect(counters(before, action)).to.be.deep.equal(after)
 })
