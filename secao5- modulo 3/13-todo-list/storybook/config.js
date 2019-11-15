7893fab777379b89adf20c954b14aa3dfd2d3e3f
George Salu <george.salu10@gmail.com>
2019-11-14 21:33:28 -0300

    incluindo comentario sobre conf do storybook

--- secao5- modulo 3/13-todo-list/storybook/config.js
+++ secao5- modulo 3/13-todo-list/storybook/config.js
@@ -1,5 +1,7 @@
 import { configure } from '@storybook/react'
 
+// arquivo de configuração da nova versao da dependecia do
+// storybook
 const req = require.context('../src/components', true, /\.story\.js$/)
 
 function loadStories () {
