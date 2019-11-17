dd5e86bcfdf757bf3001aafb2e882d21f26cebaa
George Salu <george.salu10@gmail.com>
2019-11-17 18:09:31 -0300

    App Todo List - implementação de reducer de visibilityFilter

---
+++ secao5- modulo 3/13-todo-list/src/redux-flow/reducers/visibility-filter/index.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const visibilityFilter = () => {}
+
+export default visibilityFilter
---
+++ secao5- modulo 3/13-todo-list/src/redux-flow/reducers/visibility-filter/visibility-filter.test.js
@@ -0,0 +1,8 @@
+'use strict'
+
+import visibilityFilter from './index'
+import { expect } from 'chai'
+
+it('visibilityFilter should be a function', () => {
+  expect(visibilityFilter).to.be.a('fucntion')
+})
