a09557d2c7e5f12c055243aa5a9866ed381798c8
George Salu <george.salu10@gmail.com>
2019-11-17 22:45:08 -0300

    refatorando

--- secao5- modulo 3/13-todo-list/src/redux-flow/reducers/visibility-filter/visibility-filter.test.js
+++ secao5- modulo 3/13-todo-list/src/redux-flow/reducers/visibility-filter/visibility-filter.test.js
@@ -3,51 +3,52 @@
 import visibilityFilter, { initialState } from './index'
 import { expect } from 'chai'
 import deepFeeze from 'deep-freeze'
+import { SET_VISIBILITY_FILTER, SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE } from './actions'
 
 it('visibilityFilter should be a function', () => {
   expect(visibilityFilter).to.be.a('function')
 })
 
 it('should show all todos', () => {
-  const before = 'SHOW_COMPLETED'
+  const before = SHOW_COMPLETED
   const action = deepFeeze({
-    type: 'SET_VISIBILITY_FILTER',
-    payload: { filter: 'SHOW_ALL' }
+    type: SET_VISIBILITY_FILTER,
+    payload: { filter: SHOW_ALL }
   })
-  const after = 'SHOW_ALL'
+  const after = SHOW_ALL
 
   expect(visibilityFilter(before, action)).to.be.equal(after)
 })
 
 it('should show just completed todos', () => {
-  const before = 'SHOW_ALL'
+  const before = SHOW_ALL
   const action = deepFeeze({
-    type: 'SET_VISIBILITY_FILTER',
-    payload: { filter: 'SHOW_COMPLETED' }
+    type: SET_VISIBILITY_FILTER,
+    payload: { filter: SHOW_COMPLETED }
   })
-  const after = 'SHOW_COMPLETED'
+  const after = SHOW_COMPLETED
 
   expect(visibilityFilter(before, action)).to.be.equal(after)
 })
 
 it('should show just active todos', () => {
-  const before = 'SHOW_ALL'
+  const before = SHOW_ALL
   const action = deepFeeze({
-    type: 'SET_VISIBILITY_FILTER',
-    payload: { filter: 'SHOW_ACTIVE' }
+    type: SET_VISIBILITY_FILTER,
+    payload: { filter: SHOW_ACTIVE }
   })
-  const after = 'SHOW_ACTIVE'
+  const after = SHOW_ACTIVE
 
   expect(visibilityFilter(before, action)).to.be.equal(after)
 })
 
 it('should return latest state when action is unknown', () => {
-  const before = 'SHOW_COMPLETED'
+  const before = SHOW_COMPLETED
   const action = deepFeeze({
     type: 'UNKNOWN',
-    payload: { filter: 'SHOW_ALL' }
+    payload: { filter: SHOW_ALL }
   })
-  const after = 'SHOW_COMPLETED'
+  const after = SHOW_COMPLETED
 
   expect(visibilityFilter(before, action)).to.be.equal(after)
 })
