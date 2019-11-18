ca1c75265a12a5d118d7fd2184025b209e12ee44
George Salu <george.salu10@gmail.com>
2019-11-18 14:41:44 -0300

    removendo comentarios

--- secao5- modulo 3/13-todo-list/src/redux-flow/reducers/index.js
+++ secao5- modulo 3/13-todo-list/src/redux-flow/reducers/index.js
@@ -4,13 +4,7 @@ import { combineReducers } from 'redux'
 import reducerTodos from './todos'
 import reducerVisibilityFilter from './visibility-filter'
 
-/*
-const rootReducer = (state = {}, action) => {
-  return {
-    todos: reducerTodos(state.todos, action),
-    visibilityFilter: reducerVisibilityFilter(state.visibilityFilter, action)
-  }
-} */
+
 
 export default combineReducers({
   todos: reducerTodos,
