f04217d99ab2831b9953cc9d3d758d6659c00464
George Salu <george.salu10@gmail.com>
2019-11-18 15:55:15 -0300

    App Todo List - implementação manual do combineReducers

--- secao5- modulo 3/13-todo-list/src/redux-flow/reducers/index.js
+++ secao5- modulo 3/13-todo-list/src/redux-flow/reducers/index.js
@@ -1,10 +1,17 @@
 'use strict'
 
-import { combineReducers } from 'redux'
+//import { combineReducers } from 'redux'
 import reducerTodos from './todos'
 import reducerVisibilityFilter from './visibility-filter'
 
+const combineReducers = (reducers) => (state = {}, action) => {
+  return Object.keys(reducers).reduce((nextState, key) => {
+    nextState[key] = reducers[key](state[key], action)
+    return nextState
+  }, {})
+}
+
 export default combineReducers({
-  todos: reducerTodos,
-  visibilityFilter: reducerVisibilityFilter
+  todos,
+  visibilityFilter
 })
