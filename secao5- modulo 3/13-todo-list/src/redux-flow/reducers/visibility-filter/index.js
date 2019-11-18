5e111a4014afc44f7058c9807e01dd7df064c3dc
George Salu <george.salu10@gmail.com>
2019-11-17 22:41:30 -0300

    App Todo List - implementação de reducer de visibilityFilter - parte 2

--- secao5- modulo 3/13-todo-list/src/redux-flow/reducers/visibility-filter/index.js
+++ secao5- modulo 3/13-todo-list/src/redux-flow/reducers/visibility-filter/index.js
@@ -1,7 +1,13 @@
 'use strict'
 
-const visibilityFilter = () => {
-  return 'SHOW_ALL'
+export const initialState = 'SHOW_ALL'
+
+const visibilityFilter = (state = initialState, action) => {
+  switch(action.type) {
+    case 'SET_VISIBILITY_FILTER':
+        return action.payload.filter
+  }
+  return state
 }
 
 export default visibilityFilter
--- secao5- modulo 3/13-todo-list/src/redux-flow/reducers/visibility-filter/visibility-filter.test.js
+++ secao5- modulo 3/13-todo-list/src/redux-flow/reducers/visibility-filter/visibility-filter.test.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import visibilityFilter from './index'
+import visibilityFilter, { initialState } from './index'
 import { expect } from 'chai'
 import deepFeeze from 'deep-freeze'
 
@@ -26,6 +26,36 @@ it('should show just completed todos', () => {
     payload: { filter: 'SHOW_COMPLETED' }
   })
   const after = 'SHOW_COMPLETED'
+
+  expect(visibilityFilter(before, action)).to.be.equal(after)
+})
+
+it('should show just active todos', () => {
+  const before = 'SHOW_ALL'
+  const action = deepFeeze({
+    type: 'SET_VISIBILITY_FILTER',
+    payload: { filter: 'SHOW_ACTIVE' }
+  })
+  const after = 'SHOW_ACTIVE'
+
+  expect(visibilityFilter(before, action)).to.be.equal(after)
+})
+
+it('should return latest state when action is unknown', () => {
+  const before = 'SHOW_COMPLETED'
+  const action = deepFeeze({
+    type: 'UNKNOWN',
+    payload: { filter: 'SHOW_ALL' }
+  })
+  const after = 'SHOW_COMPLETED'
+
+  expect(visibilityFilter(before, action)).to.be.equal(after)
+})
+
+it('should return initialState  when latest state is undefined', () => {
+  const before = undefined
+  const action = deepFeeze({})
+  const after = initialState
 
   expect(visibilityFilter(before, action)).to.be.equal(after)
 })
