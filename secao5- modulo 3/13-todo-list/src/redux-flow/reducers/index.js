be31872b8977132349dabb4872871c2a52744d97
George Salu <george.salu10@gmail.com>
2019-11-19 12:42:11 -0300

    usando o combineReducer do redux

--- secao5- modulo 3/13-todo-list/src/redux-flow/reducers/index.js
+++ secao5- modulo 3/13-todo-list/src/redux-flow/reducers/index.js
@@ -2,15 +2,7 @@
 
 import todos from './todos'
 import visibilityFilter from './visibility-filter'
-
-const combineReducers = (reducers) => (state = {}, action) => {
-  return Object.keys(reducers).reduce((nextState, key) => {
-    return {
-      ...nextState,
-      [key]: reducers[key](state[key], action)
-    }
-  }, {})
-}
+import { combineReducers } from 'redux'
 
 export default combineReducers({
   todos,
