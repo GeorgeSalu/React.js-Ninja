e53fab01c103d780c4ee43603eceaf9f7340b5f0
George Salu <george.salu10@gmail.com>
2019-11-19 15:59:22 -0300

    corrigindo filter-link.js

--- secao5- modulo 3/13-todo-list/src/components/filter/filter-link.js
+++ secao5- modulo 3/13-todo-list/src/components/filter/filter-link.js
@@ -3,9 +3,8 @@
 import React from 'react'
 
 const FilterLink = ({ action, activeFilter, children, onClick }) => {
-
   const span = { tag: 'span' }
-  const a = { tag: 'a', href:'#', onClick }
+  const a = { tag: 'a', href: '#', onClick }
 
   const Component = action = activeFilter ? span : a
 
