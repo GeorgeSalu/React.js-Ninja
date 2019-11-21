0e361248f367392e8a5a2513fdc15385bbdd0056
George Salu <george.salu10@gmail.com>
2019-11-21 14:03:40 -0300

    Busca Cep - Usando async no redux

---
+++ secao5- modulo 3/todo-list-get-cep/src/components/search-cep/index.js
@@ -0,0 +1,36 @@
+'use strict'
+
+import React from 'react'
+
+const SearchCep = () => (
+  <div>
+    <form>
+      <input type='text' name='cep' />
+      <button type='submit'>Buscar enderecos</button>
+    </form>
+
+    <table>
+      <thead>
+        <tr>
+          <td>Cep</td>
+          <td>Endenreco</td>
+          <td>Bairro</td>
+          <td>Cidade</td>
+          <td>Estado</td>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>999999</td>
+          <td>Endenreco</td>
+          <td>Bairro</td>
+          <td>Cidade</td>
+          <td>Estado</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+)
+
+export default SearchCep
