c6de43e0c0e390b5a874568a748a34472fe404e2
George Salu <george.salu10@gmail.com>
2019-11-21 17:18:41 -0300

    corrigindo identação

--- secao5- modulo 3/todo-list-get-cep/src/components/search-cep/search-cep.js
+++ secao5- modulo 3/todo-list-get-cep/src/components/search-cep/search-cep.js
@@ -12,42 +12,42 @@ const SearchCep = ({
   isFetching,
   handleSubmit
  }) => (
-  <div>
-    <form onSubmit={handleSubmit}>
-      <input type='text' name='cep' />
-      <button type='submit' disabled={isFetching}>
-        {isFetching ? 'Buscando....' : 'Buscar enderecos'}
-      </button>
-    </form>
+   <div>
+     <form onSubmit={handleSubmit}>
+       <input type='text' name='cep' />
+       <button type='submit' disabled={isFetching}>
+         {isFetching ? 'Buscando....' : 'Buscar enderecos'}
+       </button>
+     </form>
 
-    {status === 0 && <div>Cep nao encontrado</div>}
+     {status === 0 && <div>Cep nao encontrado</div>}
 
-    {status === 1 && (
-      <table>
-      <thead>
-        <tr>
-          <td>Cep</td>
-          <td>Endereco</td>
-          <td>Bairro</td>
-          <td>Cidade</td>
-          <td>Estado</td>
-          <td>Status</td>
-        </tr>
-      </thead>
+     {status === 1 && (
+     <table>
+       <thead>
+         <tr>
+           <td>Cep</td>
+           <td>Endereco</td>
+           <td>Bairro</td>
+           <td>Cidade</td>
+           <td>Estado</td>
+           <td>Status</td>
+         </tr>
+       </thead>
 
-      <tbody>
-        <tr>
-          <td>{code}</td>
-          <td>{address}</td>
-          <td>{district}</td>
-          <td>{city}</td>
-          <td>{state}</td>
-          <td>{status}</td>
-        </tr>
-      </tbody>
-    </table>
+       <tbody>
+         <tr>
+           <td>{code}</td>
+           <td>{address}</td>
+           <td>{district}</td>
+           <td>{city}</td>
+           <td>{state}</td>
+           <td>{status}</td>
+         </tr>
+       </tbody>
+     </table>
     )}
-  </div>
+   </div>
 )
 
 export default SearchCep
