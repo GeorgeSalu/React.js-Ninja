da154f837703d39d9e74435409b88ce63a47cfc0
George Salu <george.salu10@gmail.com>
2019-11-21 15:46:43 -0300

    outra forma de usar a lib ajax

--- secao5- modulo 3/todo-list-get-cep/src/components/search-cep/index.js
+++ secao5- modulo 3/todo-list-get-cep/src/components/search-cep/index.js
@@ -6,7 +6,7 @@ import ajax from '@fdaciuk/ajax'
 
 class SearchCepContainer extends PureComponent {
   async componentDidMount () {
-    const response = await ajax().get('http://apps.widenet.com.br/busca-cep/api/cep.json?code=06233-030')
+    const response = await ajax().get('http://apps.widenet.com.br/busca-cep/api/cep.json', { code: '06233-030' })
     console.log(response)
   }
 
