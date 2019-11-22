3fdb0522c6a84b4f41d94695ffb79858ee6fbc1c
George Salu <george.salu10@gmail.com>
2019-11-22 14:20:58 -0300

    Busca Cep - teste para createReducer - parte 2

--- secao5- modulo 3/todo-list-get-cep/src/redux-flow/reducers/create-reducer.test.js
+++ secao5- modulo 3/todo-list-get-cep/src/redux-flow/reducers/create-reducer.test.js
@@ -1,8 +1,26 @@
 'use strict'
 
 import { expect } from 'chai'
+import deepFreeze from 'deep-freeze'
 import createReducer from './create-reducer'
+
+const initialState = 0
+const reducer = createReducer(initialState, {
+  'INCREMENT': (state, action) => state + 1,
+  'DECREMENT': (state, action) => state - 1
+})
 
 it('createReducer should be a function', () => {
   expect(createReducer).to.be.a('function')
+})
+
+it('createReducer(initialState, {}) should return a reducer function', () => {
+  expect(createReducer([], {})).to.be.a('function')
+})
+
+it('should create a reducer', () => {
+  const before = 0
+  const action = deepFreeze({ type: 'INCREMENT' })
+  const after = 1
+  expect(reducer(before, action)).to.be.equal(after)
 })
