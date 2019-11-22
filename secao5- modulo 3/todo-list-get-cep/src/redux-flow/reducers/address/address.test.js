755f1fda9de65963c2d30cc8bbd62b656b36623b
George Salu <george.salu10@gmail.com>
2019-11-22 11:55:23 -0300

    Busca CEP - criar reducer de enderecos

--- secao5- modulo 3/todo-list-get-cep/src/redux-flow/reducers/address/address.test.js
+++ secao5- modulo 3/todo-list-get-cep/src/redux-flow/reducers/address/address.test.js
@@ -6,3 +6,37 @@ import address from './index'
 it('address should be a function', () => {
   expect(address).to.be.a('function')
 })
+
+it('should action UPDATE_ADDRESS update address', () => {
+  const before = {
+    address: '',
+    city: '',
+    code: '',
+    district: '',
+    state: '',
+    status: 1,
+  }
+
+  const action = {
+    type: 'UPDATE_ADDRESS',
+    payload: {
+      address: 'Avenida Presidente Kennedy - de 1632',
+      city: 'Curitiba',
+      code: '80610-01',
+      district: 'Agua Verde',
+      state: 'PR',
+      status: 1,
+    }
+  }
+
+  const after = {
+      address: 'Avenida Presidente Kennedy - de 1632',
+      city: 'Curitiba',
+      code: '80610-01',
+      district: 'Agua Verde',
+      state: 'PR',
+      status: 1,
+  }
+
+  expect(address(before, action)).to.be.deep.equal(after)
+})
--- secao5- modulo 3/todo-list-get-cep/src/redux-flow/reducers/address/index.js
+++ secao5- modulo 3/todo-list-get-cep/src/redux-flow/reducers/address/index.js
@@ -1,3 +1,18 @@
 'use strict'
 
-export default () => {}
+import createReducer  from '../create-reducer'
+
+const initialState = {
+    address: '',
+    city: '',
+    code: '',
+    district: '',
+    state: '',
+    status: 1,
+}
+
+const address = createReducer(initialState, {
+  'UPDATE_ADDRESS': (state, action) => action.payload
+})
+
+export default address
