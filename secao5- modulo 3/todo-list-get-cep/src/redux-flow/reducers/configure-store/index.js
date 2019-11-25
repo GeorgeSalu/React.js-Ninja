435c38dd178a3232ce3165a4ded4ec1da96a7533
George Salu <george.salu10@gmail.com>
2019-11-25 17:31:42 -0300

    Busca CEP - usando mas de um store enhancer no Redux

--- secao5- modulo 3/todo-list-get-cep/src/redux-flow/reducers/configure-store/index.js
+++ secao5- modulo 3/todo-list-get-cep/src/redux-flow/reducers/configure-store/index.js
@@ -5,7 +5,7 @@ import thunk from 'redux-thunk'
 import rootReducer from 'reducers'
 
 export default ({ initialState } = {}) => {
-  const enhancer = compose( applyMiddleware(logger), applyMiddleware(thunk))
+  const enhancer = compose(applyMiddleware(thunk), logger())
   const store = createStore(rootReducer, initialState, enhancer)
 
   if(module.hot) {
@@ -18,14 +18,11 @@ export default ({ initialState } = {}) => {
   return store
 }
 
-const logger = ({ dispatch, getState }) => (next) => (action) => {
-  console.group(`LOGGER->${action.type}`)
-  console.log('LOGGER:Will dispatch:', action)
-  const nextAction = next(action)
-  console.log('LOGGER:Will dispatch:', nextAction)
-  console.groupEnd(`LOGGER->${action.type}`)
-  return nextAction
-}
+const logger = () => window.__REDUX_DEVTOOLS_EXTENSION__
+  ? window.__REDUX_DEVTOOLS_EXTENSION__()
+  : (x) => x
+
+
 
 const thunk = ({ dispatch, getState }) => (next) => (action) => {
   if(typeof action === 'function') {
