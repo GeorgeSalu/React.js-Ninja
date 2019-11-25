6323aa926ba68be829b8fe983edc6673156a4374
George Salu <george.salu10@gmail.com>
2019-11-25 14:44:24 -0300

    fix for lint

--- secao5- modulo 3/todo-list-get-cep/src/redux-flow/reducers/address/action-creators.js
+++ secao5- modulo 3/todo-list-get-cep/src/redux-flow/reducers/address/action-creators.js
@@ -4,9 +4,9 @@ import { UPDATE_ADDRESS } from './actions'
 import ajax from '@fdaciuk/ajax'
 
 export const fetchAddress = (cep) => async (dispatch, getState) => {
-    const response = await ajax().get(`http://apps.widenet.com.br/busca-cep/api/cep.json?code=${cep}`)
+  const response = await ajax().get(`http://apps.widenet.com.br/busca-cep/api/cep.json?code=${cep}`)
 
-    dispatch(updateAddress(response))
+  dispatch(updateAddress(response))
 }
 
 export const updateAddress = (date) => ({
