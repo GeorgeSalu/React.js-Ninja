c2737b3120761fd41d9020e82341373d0b0f42ac
George Salu <george.salu10@gmail.com>
2019-11-25 17:05:15 -0300

    Busca CEP - usando mas de um store enhancer no Redux

--- secao5- modulo 3/todo-list-get-cep/src/redux-flow/reducers/configure-store/index.js
+++ secao5- modulo 3/todo-list-get-cep/src/redux-flow/reducers/configure-store/index.js
@@ -1,11 +1,12 @@
 'use strict'
 
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from 'reducers'
 
 export default ({ initialState } = {}) => {
-  const store = createStore(rootReducer, initialState, applyMiddleware(logger, thunk))
+  const enhancer = compose( applyMiddleware(logger), applyMiddleware(thunk))
+  const store = createStore(rootReducer, initialState, enhancer)
 
   if(module.hot) {
     module.hot.accept('reducers', () => {
