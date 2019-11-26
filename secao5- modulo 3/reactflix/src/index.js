a4d7579d6f320f261032f09cbf9c5eb183eb804e
George Salu <george.salu10@gmail.com>
2019-11-26 15:06:45 -0300

    removendo diretorio redux-flow

--- secao5- modulo 3/reactflix/src/index.js
+++ secao5- modulo 3/reactflix/src/index.js
@@ -5,16 +5,11 @@ import { render } from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 import { Provider } from 'react-redux'
 import App from './app'
-import configureStore from './redux-flow/configure-store'
-
-const store = configureStore()
 
 const renderApp = (NextApp) => {
   render(
     <AppContainer>
-      <Provider store={store}>
         <NextApp />
-      </Provider>
     </AppContainer>,
     document.querySelector('[data-js="app"]')
   )
