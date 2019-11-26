5eee3dfb836f7567b6ed08c1c6a341eb7598d1ac
George Salu <george.salu10@gmail.com>
2019-11-26 15:11:16 -0300

    removendo import nao utilizado

--- secao5- modulo 3/reactflix/src/index.js
+++ secao5- modulo 3/reactflix/src/index.js
@@ -3,7 +3,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
-import { Provider } from 'react-redux'
 import App from './app'
 
 const renderApp = (NextApp) => {
