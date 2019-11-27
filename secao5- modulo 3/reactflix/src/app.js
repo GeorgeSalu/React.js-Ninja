8d78ea23a8d979767d4bd235e022e9987dde495c
George Salu <george.salu10@gmail.com>
2019-11-26 21:19:01 -0300

    tornando o componente mas cru

--- secao5- modulo 3/reactflix/src/app.js
+++ secao5- modulo 3/reactflix/src/app.js
@@ -1,11 +1,8 @@
 'use strict'
 
-import React, { PureComponent } from 'react'
+import React, { Component } from 'react'
 
-import 'normalize.css'
-import 'milligram'
-
-class App extends PureComponent {
+class App extends Component {
   render () {
     return (
       <div>
